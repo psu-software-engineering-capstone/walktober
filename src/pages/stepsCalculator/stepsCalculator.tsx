278d5d96b42e3ce72691c616563a6e9fb58b9e35
NathanMoes <nathanmoes7@gmail.com>
2022-11-19 14:42:17 -0800

    Added more percise steps Calc Imperial only

--- src/pages/stepsCalculator/stepsCalculator.tsx
+++ src/pages/stepsCalculator/stepsCalculator.tsx
@@ -23,9 +23,14 @@ const StepsCalculator: React.FC = () => {
     const [isTouched, setIsTouched] = useState(false);
     const [isValid, setIsValid] = useState<boolean>();
     var steps: number = 0;
+    var heightIn: number = 0; // height inches param
+    var heightFt: number = 0; // height feet param
+    var strideConverter: number = 0.413; // conversion factor for height in inches to average stride in inches
+    var heightCm: number = 0.0; // for our metric friends
     var time: number;
     var stepsPerMile: number = 2250;
     var miles: number;
+    var metric: boolean = false;
 
     function calculate(ev: Event) {
         miles = Number((ev.target as HTMLInputElement).value);
@@ -36,11 +41,44 @@ const StepsCalculator: React.FC = () => {
         steps = miles * stepsPerMile;
     }
 
+    function getHeightFt(ev: Event) {
+        heightFt = Number((ev.target as HTMLInputElement).value);
+        if (heightFt <= 0) {
+            return 0;
+        }
+    }
+
+    function getHeightIn(ev: Event) {
+        heightIn = Number((ev.target as HTMLInputElement).value);
+        if (heightIn <= 0) {
+            return 0;
+        }
+    }
+
+    function getHeightcm(ev: Event) {
+        heightCm = Number((ev.target as HTMLInputElement).value);
+        if (heightCm <= 0) {
+            return 0;
+        }
+    }
+
     function placeHolder() {
         // return steps.toString();
         const updateSteps = document.querySelector('#result');
-        if (updateSteps != null)
-            updateSteps.innerHTML = steps.toString();
+        if (updateSteps != null) {
+            if (metric == false && heightFt > 0) {
+                // imperial calculations
+                // height in IN * convert = inches per stride
+                // IN per stride / 12 = feet per stride
+                // 5280 / feet per stride = steps per mile
+                steps = miles * (5280 / ((((heightFt * 12) + heightIn) * strideConverter) / 12));
+            }
+            else if (metric == true) {
+                // metric calculations
+            }
+            updateSteps.innerHTML = Math.round(steps).toString();
+        }
+        // updateSteps.innerHTML = steps.toString();
         return;
     }
 
@@ -64,6 +102,30 @@ const StepsCalculator: React.FC = () => {
                     }></IonInput>
                     <IonRouterLink slot="helper" href="./manualLoggingSteps">Return to steps logging</IonRouterLink>
                 </IonItem>
+                <IonGrid>
+                    <IonRow>
+                        <IonCol>
+                            <IonItem>
+                                <IonLabel position="floating">Optional: Height ft</IonLabel>
+                                <IonInput type="number" onInput={
+                                    (event: any) => {
+                                        getHeightFt(event);
+                                    }
+                                }></IonInput>
+                            </IonItem>
+                        </IonCol>
+                        <IonCol>
+                            <IonItem>
+                                <IonLabel position="floating">Optional: Height In</IonLabel>
+                                <IonInput type="number" onInput={
+                                    (event: any) => {
+                                        getHeightIn(event);
+                                    }
+                                }></IonInput>
+                            </IonItem>
+                        </IonCol>
+                    </IonRow>
+                </IonGrid>
                 <IonCol>
                     <IonButton onClick={placeHolder}>Submit</IonButton>
                 </IonCol>
