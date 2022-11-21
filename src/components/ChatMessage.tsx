880135f31d281d6557e9cccdb5c7355c996a0ff7
Alana Gilston <abluescarab@users.noreply.github.com>
2022-11-21 00:18:01 -0800

    Added datetime property to ChatMessage

--- src/components/ChatMessage.tsx
+++ src/components/ChatMessage.tsx
@@ -4,17 +4,19 @@ import {
     IonCardHeader,
     IonImg,
     IonCardTitle,
+    IonCardSubtitle,
 } from "@ionic/react";
 import "./ChatMessage.scss";
 
 interface ChatMessageProps {
-    sent?: boolean,   // if message was sent from this device
-    src?: string,     // profile photo src url
-    text: string,     // text of the message
-    username?: string // username of sender
+    sent?: boolean,    // if message was sent from this device
+    src?: string,      // profile photo src url
+    text: string,      // text of the message
+    username?: string, // username of sender
+    datetime: string,  // date and time message was sent
 };
 
-const ChatMessage: React.FC<ChatMessageProps> = ({ sent, src="", text, username="" }) => {
+const ChatMessage: React.FC<ChatMessageProps> = ({ sent, src="", text, username="", datetime }) => {
     return (
         <div className="chat-message-container">
             {src &&
@@ -24,6 +26,7 @@ const ChatMessage: React.FC<ChatMessageProps> = ({ sent, src="", text, username=
                 {username &&
                     <IonCardHeader>
                         <IonCardTitle>{username}</IonCardTitle>
+                        <IonCardSubtitle>{datetime}</IonCardSubtitle>
                     </IonCardHeader>
                 }
                 <IonCardContent>{text}</IonCardContent>
