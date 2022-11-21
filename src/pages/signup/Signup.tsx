9a30c2cd90f940a6ff1114537004c8789bfaf3a5
xuvea <90873349+xuvea@users.noreply.github.com>
2022-11-20 16:58:00 -0800

    Update to View
    Added part of the view for inputs and buttons. Need to add more styling for alignment and the missing Google alternate Login.

--- src/pages/signup/Signup.tsx
+++ src/pages/signup/Signup.tsx
@@ -1,6 +1,22 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../../components/ExploreContainer';
-import './Signup.css';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCardTitle,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonInput,
+  IonButton,
+  IonCardSubtitle,
+} from "@ionic/react";
+//import ExploreContainer from '../../components/ExploreContainer';
+import "./Signup.css";
 
 const Signup: React.FC = () => {
   return (
@@ -16,7 +32,47 @@ const Signup: React.FC = () => {
             <IonTitle size="large">Signup</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Signup page" />
+        <IonCard>
+          <IonCardHeader>
+            <IonCardTitle>SignUp</IonCardTitle>
+          </IonCardHeader>
+          <IonCardContent>
+            <IonList>
+              <IonItem>
+                <IonLabel>Email</IonLabel>
+                <IonInput></IonInput>
+              </IonItem>
+
+              <IonItem>
+                <IonLabel>First Name</IonLabel>
+                <IonInput></IonInput>
+              </IonItem>
+
+              <IonItem>
+                <IonLabel>Last Name</IonLabel>
+                <IonInput></IonInput>
+              </IonItem>
+
+              <IonItem>
+                <IonLabel>Password</IonLabel>
+                <IonInput></IonInput>
+              </IonItem>
+
+              <IonItem lines="none">
+                <IonLabel>Confirm Password</IonLabel>
+                <IonInput></IonInput>
+              </IonItem>
+
+              <IonItem lines="none">
+                <IonButton fill="solid" color="primary" class="test">Confirm</IonButton>
+              </IonItem>
+
+              <IonCardSubtitle>
+                <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley">Forgot Password?</a>
+              </IonCardSubtitle>
+            </IonList>
+          </IonCardContent>
+        </IonCard>
       </IonContent>
     </IonPage>
   );
