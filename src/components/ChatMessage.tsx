8b619b112418dec26d72550f8ca57f26294664df
Alana Gilston <abluescarab@users.noreply.github.com>
2022-11-21 19:05:48 -0800

    Username required on ChatMessages and displayed for sent and received

--- src/components/ChatMessage.tsx
+++ src/components/ChatMessage.tsx
@@ -9,14 +9,14 @@ import {
 import "./ChatMessage.scss";
 
 interface ChatMessageProps {
-    sent?: boolean,    // if message was sent from this device
-    src?: string,      // profile photo src url
-    text: string,      // text of the message
-    username?: string, // username of sender
-    datetime: string,  // date and time message was sent
+    sent?: boolean,   // if message was sent from this device
+    src?: string,     // profile photo src url
+    text: string,     // text of the message
+    username: string, // username of sender
+    datetime: string, // date and time message was sent
 };
 
-const ChatMessage: React.FC<ChatMessageProps> = ({ sent, src="", text, username="", datetime }) => {
+const ChatMessage: React.FC<ChatMessageProps> = ({ sent, src="", text, username, datetime }) => {
     return (
         <div className="chat-message-container">
             {src &&
@@ -24,9 +24,7 @@ const ChatMessage: React.FC<ChatMessageProps> = ({ sent, src="", text, username=
             }
             <IonCard className={sent ? "chat-message chat-message-sent" : "chat-message"} color={sent ? "primary" : "light"}>
                 <IonCardHeader>
-                    {username &&
-                        <IonCardTitle>{username}</IonCardTitle>
-                    }
+                    <IonCardTitle>{username}</IonCardTitle>
                     <IonCardSubtitle>{datetime}</IonCardSubtitle>
                 </IonCardHeader>
                 <IonCardContent>{text}</IonCardContent>
