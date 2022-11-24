1654981ec44978ca539399a1dc9323c6c1450115
NathanMoes <91927252+NathanMoes@users.noreply.github.com>
2022-11-24 09:36:08 -0800

    Finished adding Eslint in "I think"
    to run just do npx path/file.extention. Will add in scripts to make it easier. And checkout stuff for prettier

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,4 +1,4 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import React, { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
 
