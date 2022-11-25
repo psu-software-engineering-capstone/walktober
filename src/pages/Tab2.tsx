8fdf868d8a35bdf6c9bf6d80441ed389a6bbcaf9
NathanMoes <91927252+NathanMoes@users.noreply.github.com>
2022-11-25 15:21:22 -0800

    E

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,4 +1,4 @@
-import React, { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab2.css';
 
