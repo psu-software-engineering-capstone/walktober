2d25685d29f713ce9b9f1053980d83751fcf154e
bailee-crandley <crandley@pdx.edu>
2022-11-27 12:30:50 -0800

    Added profile page content using temp data

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,8 +1,64 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { 
+    IonContent, 
+    IonHeader, 
+    IonTitle, 
+    IonToolbar, 
+    IonImg, 
+    IonItem, 
+    IonText, 
+    IonGrid, 
+    IonRow, 
+    IonCol, 
+    IonButton, 
+    IonPage
+} from '@ionic/react';
 import './Profile.css';
 
 const Profile: React.FC = () => {
+    let profilePic: string;
+    let name: string;
+    let email: string;
+    let joinDate: Date;
+    let joinDateString: string;
+    let totalDistance: number; // Currently in miles
+    // let badges;
+
+    function GetRecords (){
+        // TODO: Get information from database 
+        profilePic = "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2b/Kristen_Stewart_in_2022.JPG/1280px-Kristen_Stewart_in_2022.JPG";
+        name = "Kristen Stewart";
+        email = "stewart@pdx.edu";
+        joinDate = new Date("2015-03-25");
+        joinDateString = joinDate.toLocaleDateString();
+        totalDistance = 500;
+    }
+    
+    function DisplayRecords(){
+        GetRecords()
+        return (
+            <IonGrid>
+                <IonRow>
+                    <IonCol size="auto">
+                        <IonText>
+                            <IonImg className = "ProfilePic" src={profilePic} alt="Our lord and savior Kristen Stewart"></IonImg>
+                            <h2>{name}</h2>
+                            <p>{email}</p>
+                            <IonButton>Change Password</IonButton>
+                        </IonText>
+                    </IonCol>
+                    <IonCol>
+                        <IonText>
+                            <p>Joined on {joinDateString}</p>
+                            <p>{totalDistance} miles walked in total</p>
+                            <h6>Badges:</h6>
+                            {/* TODO: Put badges here */}
+                        </IonText>
+                    </IonCol>
+                </IonRow>
+            </IonGrid>
+        )
+    }
+
   return (
     <IonPage>
       <IonHeader>
@@ -10,13 +66,8 @@ const Profile: React.FC = () => {
           <IonTitle>Profile</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Profile</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Profile" />
+      <IonContent>
+        <IonItem>{DisplayRecords()}</IonItem>
       </IonContent>
     </IonPage>
   );
