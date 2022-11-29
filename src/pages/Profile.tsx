6815d04695f24e7b11ee03ff10ebd271df5cc307
bailee-crandley <crandley@pdx.edu>
2022-11-28 21:52:59 -0800

    Added missing elements

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -10,13 +10,16 @@ import {
     IonRow, 
     IonCol, 
     IonButton, 
-    IonPage
+    IonPage,
+    IonLabel,
+    IonInput
 } from '@ionic/react';
 import './Profile.css';
 
 const Profile: React.FC = () => {
     let profilePic: string;
     let name: string;
+    let username: string;
     let email: string;
     let joinDate: Date;
     let joinDateString: string;
@@ -27,6 +30,7 @@ const Profile: React.FC = () => {
         // TODO: Get information from database 
         profilePic = "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2b/Kristen_Stewart_in_2022.JPG/1280px-Kristen_Stewart_in_2022.JPG";
         name = "Kristen Stewart";
+        username = "kStewart0409"
         email = "stewart@pdx.edu";
         joinDate = new Date("2015-03-25");
         joinDateString = joinDate.toLocaleDateString();
@@ -34,29 +38,45 @@ const Profile: React.FC = () => {
     }
     
     function DisplayRecords(){
-        GetRecords()
-        return (
-            <IonGrid>
-                <IonRow>
-                    <IonCol size="auto">
-                        <IonText>
-                            <IonImg className = "ProfilePic" src={profilePic} alt="Our lord and savior Kristen Stewart"></IonImg>
-                            <h2>{name}</h2>
-                            <p>{email}</p>
-                            <IonButton>Change Password</IonButton>
-                        </IonText>
-                    </IonCol>
-                    <IonCol>
-                        <IonText>
-                            <p>Joined on {joinDateString}</p>
-                            <p>{totalDistance} miles walked in total</p>
-                            <h6>Badges:</h6>
-                            {/* TODO: Put badges here */}
-                        </IonText>
-                    </IonCol>
-                </IonRow>
-            </IonGrid>
-        )
+      GetRecords()
+      return (
+        <IonGrid>
+          <IonRow>
+            <IonCol size="auto">
+              <IonText>
+                <IonImg className = "ProfilePic" src={profilePic} alt="Our lord and savior Kristen Stewart"></IonImg>
+                <h2>{name}</h2>
+                <p>{username}</p>
+                <p>{email}</p>
+              </IonText>
+            </IonCol>
+            <IonCol>
+              <IonText>
+                <p>Joined on {joinDateString}</p>
+                <p>{totalDistance} miles walked in total</p>
+                <IonLabel>Step Goal: </IonLabel>
+                <IonItem fill="outline">
+                  <IonInput value="10,000" size={100}></IonInput>
+                </IonItem>
+                <h6>Badges:</h6>
+                {/* TODO: Put badges here */}
+              </IonText>
+            </IonCol>
+          </IonRow>
+          <IonRow>
+            <IonButton>Change Password</IonButton>
+          </IonRow>
+          <IonRow>
+            <IonButton>Change Profile Picture</IonButton>
+          </IonRow>
+          <IonRow>
+            <IonButton>Change Username</IonButton>
+          </IonRow>
+          <IonRow>
+            <IonButton>Change Health App Preferences</IonButton>
+          </IonRow>
+        </IonGrid>
+      )
     }
 
   return (
