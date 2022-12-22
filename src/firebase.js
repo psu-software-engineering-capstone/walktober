aaf6b587bc255e4008c48e17279f489a1effa1de
NathanMoes <91927252+NathanMoes@users.noreply.github.com>
2022-12-22 13:26:37 -0800

    fixing firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,20 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "@firebase/firestore";
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from '@firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCoKmeyLCWdjWgu5gXLFJjyLi1JsveYanQ",
-  authDomain: "psuwalktober.firebaseapp.com",
-  projectId: "psuwalktober",
-  storageBucket: "psuwalktober.appspot.com",
-  messagingSenderId: "949612719811",
-  appId: "1:949612719811:web:1c3329349a6538e6197c76",
-  measurementId: "G-Q4NLD3BN06"
+  apiKey: 'AIzaSyCoKmeyLCWdjWgu5gXLFJjyLi1JsveYanQ',
+  authDomain: 'psuwalktober.firebaseapp.com',
+  projectId: 'psuwalktober',
+  storageBucket: 'psuwalktober.appspot.com',
+  messagingSenderId: '949612719811',
+  appId: '1:949612719811:web:1c3329349a6538e6197c76',
+  measurementId: 'G-Q4NLD3BN06'
 };
 
+
+
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+export const RealtimeDB = getDatabase(app);
+export const FirestoreDB = getFirestore(app);
 export const auth = getAuth(app);
