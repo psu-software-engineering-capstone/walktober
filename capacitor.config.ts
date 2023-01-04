08c8ab9bfa228dea66051cce04a350f2c451230b
Juhwan <ljh1230vvv@gmail.com>
2023-01-04 04:12:37 -0800

    Improved android functionality, google auth issue unresolved

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,7 +4,14 @@ const config: CapacitorConfig = {
   appId: 'com.psu.walktober',
   appName: 'Walktober',
   webDir: 'build',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
+  plugins: {
+    GoogleAuth: {
+      scopes: ['profile', 'email'],
+      serverClientId: '949612719811-m9ptfp5jos5edh8hc2ce5c7fo1ae99qq.apps.googleusercontent.com',
+      forceCodeForRefreshToken: true
+    }
+  }
 };
 
 export default config;
