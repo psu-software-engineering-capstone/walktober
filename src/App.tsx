a3b0e316700ab8acfd76fd506412d5489b612493
NathanMoes <91927252+NathanMoes@users.noreply.github.com>
2023-01-11 17:51:19 -0800

    added alt to image for psu icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import { Redirect, Route } from 'react-router-dom';
 import {
-  IonApp,
-  IonIcon,
-  IonLabel,
-  IonRouterOutlet,
-  IonTabBar,
-  IonTabButton,
-  IonTabs,
-  IonToolbar,
-  setupIonicReact,
-  isPlatform
+	IonApp,
+	IonIcon,
+	IonLabel,
+	IonRouterOutlet,
+	IonTabBar,
+	IonTabButton,
+	IonTabs,
+	IonToolbar,
+	setupIonicReact,
+	isPlatform,
 } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import { ellipse, square, triangle } from 'ionicons/icons';
@@ -42,89 +42,97 @@ import HealthApp from './pages/HealthApp';
 setupIonicReact();
 
 const App: React.FC = () => {
-  if(!isPlatform("android") && !isPlatform("ios")) {
-    return (
-      <IonApp>
-        <IonReactRouter>
-          <IonRouterOutlet>
-            <Route exact path="/tab1">
-              <Tab1 />
-            </Route>
-            <Route exact path="/tab2">
-              <Tab2 />
-            </Route>
-            <Route path="/tab3">
-              <Tab3 />
-            </Route>
-            <Route exact path="/healthapp" >
-              <HealthApp />
-            </Route>
-            <Route exact path="/">
-              <Redirect to="/tab1" />
-            </Route>
-          </IonRouterOutlet>
-          <IonToolbar>
-            <img id="psu-logo" src="assets/logo.svg" slot="start" />
-            <div slot="end">
-              <NavLink id="home" text="Home" href="/tab1" />
-              <NavLink id="team" text="Team" href="/tab2" />
-              <NavLink id="profile" text="Profile" href="/profile">
-                <NavLink id="logs" text="Logs" href="/tab3" />
-              </NavLink>
-              <NavLink id="admin" text="Admin" href="/admin">
-                <NavLink id="admin-announcements" text="Announcements" href="/admin/announcements" />
-              </NavLink>
-            </div>
-          </IonToolbar>
-        </IonReactRouter>
-      </IonApp>
-    );
-  }
-  else {
-    return (
-      <IonApp>
-      <IonReactRouter>
-        <IonTabs>
-          <IonRouterOutlet>
-            <Route exact path="/tab1">
-              <Tab1 />
-            </Route>
-            <Route exact path="/tab2">
-              <Tab2 />
-            </Route>
-            <Route path="/tab3">
-              <Tab3 />
-            </Route>
-            <Route exact path="/healthapp" >
-              <HealthApp />
-            </Route>
-            <Route exact path="/">
-              <Redirect to="/tab1" />
-            </Route>
-          </IonRouterOutlet>
-          <IonTabBar slot="bottom">
-            <IonTabButton tab="tab1" href="/tab1">
-              <IonIcon icon={triangle} />
-              <IonLabel>Tab 1</IonLabel>
-            </IonTabButton>
-            <IonTabButton tab="tab2" href="/tab2">
-              <IonIcon icon={ellipse} />
-              <IonLabel>Tab 2</IonLabel>
-            </IonTabButton>
-            <IonTabButton tab="tab3" href="/tab3">
-              <IonIcon icon={square} />
-              <IonLabel>Tab 3</IonLabel>
-            </IonTabButton>
-            <IonTabButton tab="healthapp" href="/healthapp">
-              <IonIcon icon={square} />
-              <IonLabel>Health App Integration</IonLabel>
-            </IonTabButton>
-          </IonTabBar>
-        </IonTabs>
-      </IonReactRouter>
-    </IonApp>
-    );
-  }
+	if (!isPlatform('android') && !isPlatform('ios')) {
+		return (
+			<IonApp>
+				<IonReactRouter>
+					<IonRouterOutlet>
+						<Route exact path="/tab1">
+							<Tab1 />
+						</Route>
+						<Route exact path="/tab2">
+							<Tab2 />
+						</Route>
+						<Route path="/tab3">
+							<Tab3 />
+						</Route>
+						<Route exact path="/healthapp">
+							<HealthApp />
+						</Route>
+						<Route exact path="/">
+							<Redirect to="/tab1" />
+						</Route>
+					</IonRouterOutlet>
+					<IonToolbar>
+						<img
+							id="psu-logo"
+							src="assets/logo.svg"
+							slot="start"
+							alt="psu logo"
+						/>
+						<div slot="end">
+							<NavLink id="home" text="Home" href="/tab1" />
+							<NavLink id="team" text="Team" href="/tab2" />
+							<NavLink id="profile" text="Profile" href="/profile">
+								<NavLink id="logs" text="Logs" href="/tab3" />
+							</NavLink>
+							<NavLink id="admin" text="Admin" href="/admin">
+								<NavLink
+									id="admin-announcements"
+									text="Announcements"
+									href="/admin/announcements"
+								/>
+							</NavLink>
+						</div>
+					</IonToolbar>
+				</IonReactRouter>
+			</IonApp>
+		);
+	} else {
+		return (
+			<IonApp>
+				<IonReactRouter>
+					<IonTabs>
+						<IonRouterOutlet>
+							<Route exact path="/tab1">
+								<Tab1 />
+							</Route>
+							<Route exact path="/tab2">
+								<Tab2 />
+							</Route>
+							<Route path="/tab3">
+								<Tab3 />
+							</Route>
+							<Route exact path="/healthapp">
+								<HealthApp />
+							</Route>
+							<Route exact path="/">
+								<Redirect to="/tab1" />
+							</Route>
+						</IonRouterOutlet>
+						<IonTabBar slot="bottom">
+							<IonTabButton tab="tab1" href="/tab1">
+								<IonIcon icon={triangle} />
+								<IonLabel>Tab 1</IonLabel>
+							</IonTabButton>
+							<IonTabButton tab="tab2" href="/tab2">
+								<IonIcon icon={ellipse} />
+								<IonLabel>Tab 2</IonLabel>
+							</IonTabButton>
+							<IonTabButton tab="tab3" href="/tab3">
+								<IonIcon icon={square} />
+								<IonLabel>Tab 3</IonLabel>
+							</IonTabButton>
+							<IonTabButton tab="healthapp" href="/healthapp">
+								<IonIcon icon={square} />
+								<IonLabel>Health App Integration</IonLabel>
+							</IonTabButton>
+						</IonTabBar>
+					</IonTabs>
+				</IonReactRouter>
+			</IonApp>
+		);
+	}
 };
 
 export default App;
