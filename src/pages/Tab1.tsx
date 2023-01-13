ae29f51d792467b4fa5234e4da386a235c9f9f55
NathanMoes <91927252+NathanMoes@users.noreply.github.com>
2023-01-13 10:44:56 -0800

    Fixed some testing code that wasnt removed

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,6 +1,4 @@
-import { useContext, useState } from 'react';
 import {
-  IonButton,
   IonContent,
   IonHeader,
   IonPage,
@@ -9,12 +7,8 @@ import {
 } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
-import AuthContext from '../store/auth-context';
 
 const Tab1: React.FC = () => {
-  const ctx = useContext(AuthContext);
-  const [loggedIn, setIsLoggedIn] = useState(ctx.isLoggedIn);
-
   return (
     <IonPage>
       <IonHeader>
@@ -23,13 +17,6 @@ const Tab1: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        {loggedIn ? <h1>logged in</h1> : <h1>Not Logged in</h1>}
-        <IonButton
-          onClick={(event: any) => {
-            ctx.isLoggedIn = !ctx.isLoggedIn;
-            setIsLoggedIn(ctx.isLoggedIn);
-          }}
-        ></IonButton>
         <IonHeader collapse="condense">
           <IonToolbar>
             <IonTitle size="large">Tab 1</IonTitle>
