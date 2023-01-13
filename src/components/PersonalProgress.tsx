3e4989301dc7a0fa6ae1843998db4b33c6020a2c
NathanMoes <91927252+NathanMoes@users.noreply.github.com>
2023-01-13 14:44:19 -0800

    Added progress bar

--- src/components/PersonalProgress.tsx
+++ src/components/PersonalProgress.tsx
@@ -1,5 +1,5 @@
 import React, { ReactElement, useEffect } from 'react';
-import { IonItem } from '@ionic/react';
+import { IonItem, IonProgressBar } from '@ionic/react';
 
 const today = new Date();
 const daysLeft = 31 - today.getUTCDate(); // 31 days in october
@@ -7,20 +7,25 @@ const daysLeft = 31 - today.getUTCDate(); // 31 days in october
 const PersonalProgress: React.FC<{}> = (): ReactElement => {
   const goalSteps = 1000;
   useEffect(() => {}, []); // will be used to pull the persons goal steps
-  const currentSteps = 0;
+  const currentSteps = 200;
   useEffect(() => {}, []); // will be used to get the persons current steps
   return (
     <>
-      <p>Goal: {goalSteps}</p>
-      <hr />
-      <p>Current steps: {currentSteps}</p>
-      <hr />
-      <p>Steps needed to hit goal: {goalSteps - currentSteps}</p>
-      <hr />
-      <p>
-        average steps needed to hit goal:{' '}
-        {(goalSteps - currentSteps) / daysLeft}
-      </p>
+      <IonItem>
+        <p>Goal: {goalSteps}</p>
+        <hr />
+        <p>Current steps: {currentSteps}</p>
+        <hr />
+        <p>Steps needed to hit goal: {goalSteps - currentSteps}</p>
+        <hr />
+        <p>
+          average steps needed to hit goal:{' '}
+          {(goalSteps - currentSteps) / daysLeft}
+        </p>
+      </IonItem>
+      <IonProgressBar
+        value={1 - (goalSteps - currentSteps) / goalSteps / 1}
+      ></IonProgressBar>
     </>
   );
 };
