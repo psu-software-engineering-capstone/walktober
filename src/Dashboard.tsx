39bbd1045e4fee73cf451ebbd8afe5c5314dafdf
Alana Gilston <abluescarab@users.noreply.github.com>
2023-01-19 01:34:02 -0800

    Hides tabs (makes IonRouterOutlet work on desktop)

--- src/Dashboard.tsx
+++ src/Dashboard.tsx
@@ -20,10 +20,9 @@ import HealthApp from './pages/healthapp/HealthApp';
 import './theme/app.scss';
 
 const Dashboard: React.FC = () => {
-  if (!isPlatform('android') && !isPlatform('ios')) {
-    // if on desktop, only include the navigation router, which is then accessed
-    // using NavBar elements on individual tabs
-    return (
+  const tabsVisible = isPlatform('android') || isPlatform('ios');
+  return (
+    <IonTabs>
       <IonRouterOutlet>
         <Route exact path="/app/tab1" component={Tab1} />
         <Route exact path="/app/tab2" component={Tab2} />
@@ -33,40 +32,26 @@ const Dashboard: React.FC = () => {
           <Redirect to="/app/tab1" />
         </Route>
       </IonRouterOutlet>
-    );
-  } else {
-    return (
-      <IonTabs>
-        <IonRouterOutlet>
-          <Route exact path="/app/tab1" component={Tab1} />
-          <Route exact path="/app/tab2" component={Tab2} />
-          <Route exact path="/app/tab3" component={Tab3} />
-          <Route exact path="/app/healthapp" component={HealthApp} />
-          <Route exact path="/app">
-            <Redirect to="/app/tab1" />
-          </Route>
-        </IonRouterOutlet>
-        <IonTabBar slot="bottom">
-          <IonTabButton tab="tab1" href="/app/tab1">
-            <IonIcon icon={triangle} />
-            <IonLabel>Tab 1</IonLabel>
-          </IonTabButton>
-          <IonTabButton tab="tab2" href="/app/tab2">
-            <IonIcon icon={ellipse} />
-            <IonLabel>Tab 2</IonLabel>
-          </IonTabButton>
-          <IonTabButton tab="tab3" href="/app/tab3">
-            <IonIcon icon={square} />
-            <IonLabel>Tab 3</IonLabel>
-          </IonTabButton>
-          <IonTabButton tab="healthapp" href="/app/healthapp">
-            <IonIcon icon={square} />
-            <IonLabel>Health App Integration</IonLabel>
-          </IonTabButton>
-        </IonTabBar>
-      </IonTabs>
-    );
-  }
+      <IonTabBar slot="bottom" className={tabsVisible ? '' : 'hidden'}>
+        <IonTabButton tab="tab1" href="/app/tab1">
+          <IonIcon icon={triangle} />
+          <IonLabel>Tab 1</IonLabel>
+        </IonTabButton>
+        <IonTabButton tab="tab2" href="/app/tab2">
+          <IonIcon icon={ellipse} />
+          <IonLabel>Tab 2</IonLabel>
+        </IonTabButton>
+        <IonTabButton tab="tab3" href="/app/tab3">
+          <IonIcon icon={square} />
+          <IonLabel>Tab 3</IonLabel>
+        </IonTabButton>
+        <IonTabButton tab="healthapp" href="/app/healthapp">
+          <IonIcon icon={square} />
+          <IonLabel>Health App Integration</IonLabel>
+        </IonTabButton>
+      </IonTabBar>
+    </IonTabs>
+  );
 };
 
 export default Dashboard;
