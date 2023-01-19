cfa632c2768c54a61829bad7eb447faf0aa7fe1c
Nathan Moes <nathanmoes7@gmail.com>
2023-01-18 16:57:21 -0800

    super basic graph added for testing

--- src/components/ProgressChart.tsx
+++ src/components/ProgressChart.tsx
@@ -1,7 +1,44 @@
 import React from 'react';
+import Chart from 'chart.js/auto';
+import { Bar } from 'react-chartjs-2';
+
+const apple = (): void => {
+  const barChart = new Chart(CHART, {
+    type: 'bar',
+    data: {
+      labels: ['mon', 'tue', 'wed', 'thrs', 'fry', 'sat', 'sun'],
+      datasets: [
+        {
+          label: 'Number of steps',
+          backgroundColor: '#00ff00',
+          borderColor: '#00ff00',
+          borderWidth: 2,
+          data: [12, 10, 20, 50, 15, 16, 11]
+        }
+      ]
+    }
+  });
+};
 
 const ProgressChart: React.FC<{}> = () => {
-  return <></>;
+  return (
+    <>
+      <Bar
+        data={{
+          labels: ['mon', 'tue', 'wed', 'thrs', 'fry', 'sat', 'sun'],
+          datasets: [
+            {
+              label: 'Number of steps',
+              backgroundColor: '#00ff00',
+              borderColor: '#00ff00',
+              borderWidth: 2,
+              data: [12, 10, 20, 30, 15, 16, 11]
+            }
+          ]
+        }}
+      ></Bar>
+    </>
+  );
 };
 
 export default ProgressChart;
--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,11 +1,8 @@
-import {
-  IonContent,
-  IonPage,
-  IonTitle
-} from '@ionic/react';
+import { IonContent, IonPage, IonTitle } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
 import NavBar from '../components/NavBar';
+import ProgressChart from '../components/ProgressChart';
 
 const Tab1: React.FC = () => {
   return (
@@ -17,6 +14,7 @@ const Tab1: React.FC = () => {
         <NavBar collapse="condense">
           <IonTitle size="large">Tab 1</IonTitle>
         </NavBar>
+        <ProgressChart />
         <ExploreContainer name="Tab 1 page" />
       </IonContent>
     </IonPage>
