954339714e47b616ee845cf869643c5fb8a2fa2e
Nathan Moes <nathanmoes7@gmail.com>
2023-01-18 17:17:34 -0800

    props now forms data

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -3,6 +3,36 @@ import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
 import NavBar from '../components/NavBar';
 import ProgressChart from '../components/ProgressChart';
+const data = [
+  {
+    day: 'monday',
+    steps: 20
+  },
+  {
+    day: 'tuesday',
+    steps: 30
+  },
+  {
+    day: 'wednesday',
+    steps: 10
+  },
+  {
+    day: 'thursday',
+    steps: 50
+  },
+  {
+    day: 'friday',
+    steps: 24
+  },
+  {
+    day: 'saturday',
+    steps: 100
+  },
+  {
+    day: 'sunday',
+    steps: 21
+  }
+];
 
 const Tab1: React.FC = () => {
   return (
@@ -14,7 +44,7 @@ const Tab1: React.FC = () => {
         <NavBar collapse="condense">
           <IonTitle size="large">Tab 1</IonTitle>
         </NavBar>
-        <ProgressChart />
+        <ProgressChart data={data} />
         <ExploreContainer name="Tab 1 page" />
       </IonContent>
     </IonPage>
