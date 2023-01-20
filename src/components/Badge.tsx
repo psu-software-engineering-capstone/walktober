1d65cd4519496021624859501385372de45142f9
Nathan Moes <nathanmoes7@gmail.com>
2023-01-19 17:08:06 -0800

    Create callable element and add to tab 1 for testing

---
+++ src/components/Badge.tsx
@@ -0,0 +1,12 @@
+import { IonItem } from '@ionic/react';
+import React from 'react';
+
+const Badge: React.FC<{}> = () => {
+  return (
+    <>
+      <IonItem>testing:</IonItem>
+    </>
+  );
+};
+
+export default Badge;
--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,11 +1,8 @@
-import {
-  IonContent,
-  IonPage,
-  IonTitle
-} from '@ionic/react';
+import { IonContent, IonPage, IonTitle } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
 import NavBar from '../components/NavBar';
+import Badge from '../components/Badge';
 
 const Tab1: React.FC = () => {
   return (
@@ -17,6 +14,7 @@ const Tab1: React.FC = () => {
         <NavBar collapse="condense">
           <IonTitle size="large">Tab 1</IonTitle>
         </NavBar>
+        <Badge></Badge>
         <ExploreContainer name="Tab 1 page" />
       </IonContent>
     </IonPage>
