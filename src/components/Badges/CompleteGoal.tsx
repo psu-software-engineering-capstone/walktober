222dbcc97bfafb4f6846c296f5f7ebc41827c2f9
Nathan Moes <nathanmoes7@gmail.com>
2023-01-23 13:29:18 -0800

    Completed steps goal badge

---
+++ src/components/Badges/CompleteGoal.tsx
@@ -0,0 +1,42 @@
+import { IonItem, IonLabel, IonGrid, IonRow, IonCol } from '@ionic/react';
+import React from 'react';
+import './BadgeStyles.css';
+
+const GoalComplete: React.FC<{}> = () => {
+  return (
+    <IonItem>
+      <IonGrid>
+        <IonRow>
+          <IonCol>
+            <IonLabel>
+              <img
+                src="https://cdn-icons-png.flaticon.com/512/3050/3050426.png"
+                alt="goal reached!"
+                width={40}
+                height={40}
+              />
+            </IonLabel>
+          </IonCol>
+          <IonCol>
+            Summit reached
+            <IonItem class="p">
+              You set a steps goal for the event and reached it,
+              congradulations!
+            </IonItem>
+          </IonCol>
+        </IonRow>
+      </IonGrid>
+    </IonItem>
+  );
+};
+
+const Attribute: React.FC<{}> = () => {
+  return (
+    <a href="https://www.flaticon.com/free-icons/diamond" title="diamond icons">
+      Diamond icons created by Freepik - Flaticon
+    </a>
+  );
+};
+
+export default GoalComplete;
+export { Attribute };
