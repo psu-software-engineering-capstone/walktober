f87bb23b2d9c8083eb1c874ff99d279c2e90a107
Nathan Moes <nathanmoes7@gmail.com>
2023-01-23 11:28:54 -0800

    Removed redundent function and some imports

--- src/components/ProgressChart.tsx
+++ src/components/ProgressChart.tsx
@@ -1,30 +1,11 @@
 import React from 'react';
-import Chart from 'chart.js/auto';
-import { Bar, Line } from 'react-chartjs-2';
+import { Bar } from 'react-chartjs-2';
 import { IonLabel, IonProgressBar } from '@ionic/react';
 
 interface dataProps {
   day: string;
   steps: number;
 }
-
-const apple = (): void => {
-  const barChart = new Chart(CHART, {
-    type: 'bar',
-    data: {
-      labels: ['mon', 'tue', 'wed', 'thrs', 'fry', 'sat', 'sun'],
-      datasets: [
-        {
-          label: 'Number of steps',
-          backgroundColor: '#00ff00',
-          borderColor: '#00ff00',
-          borderWidth: 2,
-          data: [12, 10, 20, 50, 15, 16, 11]
-        }
-      ]
-    }
-  });
-};
 
 const stepsLeft = 500;
 const goalSteps = 1000;
