5c77bbf265fb86acb0a23cf9ff91bd1b7c5a0951
Nathan Moes <nathanmoes7@gmail.com>
2023-01-24 15:08:00 -0800

    fix tab1

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -2,13 +2,6 @@ import { IonContent, IonPage, IonTitle } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
 import NavBar from '../components/NavBar';
-import Badge from '../components/Badge';
-import Diamond, { Attribute } from '../components/Badges/Diamond';
-import GoalComplete from '../components/Badges/CompleteGoal';
-import TeamPlayer from '../components/Badges/TeamPlayer';
-import MarathonBadge from '../components/Badges/Marathon';
-import ConnectFitnessTracker from '../components/Badges/ConnectFitnessTracker';
-import FirstSteps from '../components/Badges/FirstSteps';
 
 const Tab1: React.FC = () => {
   return (
@@ -20,12 +13,6 @@ const Tab1: React.FC = () => {
         <NavBar collapse="condense">
           <IonTitle size="large">Tab 1</IonTitle>
         </NavBar>
-        <Badge></Badge>
-        <FirstSteps />
-        <TeamPlayer />
-        <ConnectFitnessTracker />
-        <GoalComplete />
-        <MarathonBadge />
         <ExploreContainer name="Tab 1 page" />
       </IonContent>
     </IonPage>
