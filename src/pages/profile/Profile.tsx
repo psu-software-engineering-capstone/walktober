9c6e3c23103a836477b02d28784340fb5ac90e54
Nathan Moes <nathanmoes7@gmail.com>
2023-01-26 12:28:56 -0800

    implemented useState and useEffect

--- src/pages/profile/Profile.tsx
+++ src/pages/profile/Profile.tsx
@@ -1,74 +1,50 @@
+/* eslint-disable @typescript-eslint/space-before-function-paren */
+import { useEffect, useState } from 'react';
 import {
-  IonButton, 
-  IonCol,   
-  IonContent, 
-  IonGrid, 
-  IonHeader, 
-  IonImg,   
+  IonButton,
+  IonCol,
+  IonContent,
+  IonGrid,
+  IonHeader,
+  IonImg,
   IonInput,
-  IonItem, 
+  IonItem,
   IonLabel,
   IonPage,
-  IonRow, 
-  IonText, 
-  IonTitle, 
+  IonRow,
+  IonText,
+  IonTitle,
   IonToolbar
 } from '@ionic/react';
 import './Profile.css';
 
 const Profile: React.FC = () => {
-    let email: string;    
-    let joinDate: Date;
-    let joinDateString: string;
-    let name: string;
-    let profilePic: string;
-    let totalDistance: number; // Currently in miles
-    let username: string;
-    // let badges;
+  const [email, setEmail] = useState('');
+  // let email: string;
+  const [joinDate, setJoinDate] = useState(new Date());
+  const [joinDateString, SetJoinDateString] = useState('');
+  const [name, SetName] = useState('');
+  const [profilePic, setProfilePic] = useState('');
+  const [totalDistance, SetTotalDistance] = useState(0);
+  const [username, SetUsername] = useState('');
+  // let badges;
 
-    function GetRecords (){
-        // TODO: Get information from database 
-        profilePic = "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2b/Kristen_Stewart_in_2022.JPG/1280px-Kristen_Stewart_in_2022.JPG";
-        name = "Kristen Stewart";
-        username = "kStewart0409"
-        email = "stewart@pdx.edu";
-        joinDate = new Date("2015-03-25");
-        joinDateString = joinDate.toLocaleDateString();
-        totalDistance = 500;
-    }
-    
-    function DisplayRecords(){
-      GetRecords()
-      return (
-        <IonGrid>
-          <IonRow>
-            <IonCol size="auto">
-              <IonText>
-                <IonImg className = "profile_pic" src={profilePic} alt="Profile picture for the user signed in"></IonImg>
-              <IonButton>Change Profile Picture</IonButton>
-                <h2>{name}</h2>
-                <p>{username}<IonButton fill="clear" size="small">Change Username</IonButton></p>
-                <p>{email}</p>
-                <IonButton>Change Password</IonButton> <br></br>
-                <IonButton>Change Health App Preferences</IonButton>
-              </IonText>
-            </IonCol>
-            <IonCol>
-              <IonText>
-                <p>Joined on {joinDateString}</p>
-                <p>{totalDistance} miles walked in total</p>
-                <IonLabel>Step Goal: </IonLabel>
-                <IonItem fill="outline">
-                  <IonInput value="10,000" size={100}></IonInput>
-                </IonItem>
-                <h6>Badges:</h6>
-                {/* TODO: Put badges here */}
-              </IonText>
-            </IonCol>
-          </IonRow>
-        </IonGrid>
-      )
-    }
+  function GetRecords(): void {
+    // TODO: Get information from database
+    setProfilePic(
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2b/Kristen_Stewart_in_2022.JPG/1280px-Kristen_Stewart_in_2022.JPG'
+    );
+    SetName('Kristen Stewart');
+    SetUsername('kStewart0409');
+    setEmail('stewart@pdx.edu');
+    setJoinDate(new Date('2015-03-25'));
+    SetJoinDateString(joinDate.toLocaleDateString());
+    SetTotalDistance(500);
+  }
+
+  useEffect(() => {
+    GetRecords();
+  }, [GetRecords]);
 
   return (
     <IonPage>
@@ -78,7 +54,44 @@ const Profile: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent>
-        <IonItem>{DisplayRecords()}</IonItem>
+        <IonItem>
+          <IonGrid>
+            <IonRow>
+              <IonCol size="auto">
+                <IonText>
+                  <IonImg
+                    className="profile_pic"
+                    src={profilePic}
+                    alt="Profile picture for the user signed in"
+                  ></IonImg>
+                  <IonButton>Change Profile Picture</IonButton>
+                  <h2>{name}</h2>
+                  <p>
+                    {username}
+                    <IonButton fill="clear" size="small">
+                      Change Username
+                    </IonButton>
+                  </p>
+                  <p>{email}</p>
+                  <IonButton>Change Password</IonButton> <br></br>
+                  <IonButton>Change Health App Preferences</IonButton>
+                </IonText>
+              </IonCol>
+              <IonCol>
+                <IonText>
+                  <p>Joined on {joinDateString}</p>
+                  <p>{totalDistance} miles walked in total</p>
+                  <IonLabel>Step Goal: </IonLabel>
+                  <IonItem fill="outline">
+                    <IonInput value="10,000" size={100}></IonInput>
+                  </IonItem>
+                  <h6>Badges:</h6>
+                  {/* TODO: Put badges here */}
+                </IonText>
+              </IonCol>
+            </IonRow>
+          </IonGrid>
+        </IonItem>
       </IonContent>
     </IonPage>
   );
