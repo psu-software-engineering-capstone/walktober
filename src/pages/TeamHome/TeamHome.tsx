9ee45b185b6b6c9ec2a31d1e53e33cf3b4108f7b
Alana Gilston <abluescarab@users.noreply.github.com>
2023-01-28 12:46:56 -0800

    Added to-do for changing channel based on current team

--- src/pages/TeamHome/TeamHome.tsx
+++ src/pages/TeamHome/TeamHome.tsx
@@ -13,6 +13,15 @@ const TeamHome: React.FC = () => {
         <NavBar collapse="condense">
           <IonTitle size="large">My Team</IonTitle>
         </NavBar>
+        { /*
+           * TODO: change channel based on current team
+           *
+           * To find channel ID:
+           * 1. Discord user settings (next to mute/deafen buttons) > Advanced
+           *      > enable "Developer Mode"
+           * 2. Right-click on channel name in server
+           * 3. Click "Copy ID"
+           */ }
         <WidgetBot className="discord-widget"
           server="1068966007886069841"
           channel="1068966009106600110"
