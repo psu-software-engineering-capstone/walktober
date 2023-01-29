015574038bf241f70ef86eecd948fa916dc5e485
Kevin <2.kevincarroll@gmail.com>
2023-01-29 14:44:20 -0800

    modify eslint file name, revert type to module

---
+++ .eslintrc.cjs
@@ -0,0 +1,44 @@
+
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  overrides: [
+    {
+      files: ['*.tsx', '*.ts'],
+      rules: {
+        semi: ['error', 'always'],
+        '@typescript-eslint/semi': ['error', 'always'],
+        'react/react-in-jsx-scope': 'off',
+        'react/jsx-uses-react': 'off',
+        '@typescript-eslint/no-empty-function': 'off',
+        "@typescript-eslint/consistent-type-imports": "off"
+      }
+    }
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    "ecmaFeatures": {
+      "jsx": true
+    },
+    sourceType: 'module',
+    project: ['./tsconfig.json', '*', '*.*.*'],
+    tsconfigRootDir: __dirname
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint'
+  ],
+  rules: {
+    semi: ['error', 'always'],
+    '@typescript-eslint/semi': ['error', 'always']
+  }
+};
