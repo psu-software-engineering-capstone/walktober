c0f33bfe8d7d6bef8e17e9045d7f4c92d484b777
Nathan Moes <nathanmoes7@gmail.com>
2023-02-01 13:53:56 -0800

    displays current october month's days in align with current years october

--- src/components/CalendarLeafs.tsx
+++ src/components/CalendarLeafs.tsx
@@ -1,7 +1,80 @@
+import { IonCol, IonGrid, IonItem, IonRow } from '@ionic/react';
 import React from 'react';
 
-const CalendarLeafs: React.FC<{}> = () => {
-  return <> </>;
+const currentDate = new Date();
+const currentYear = currentDate.getFullYear();
+const monthStart = new Date(`October 1, ${currentYear}`);
+
+const populateCalendar = () => {
+  let number = 1;
+
+  return (
+    <IonGrid>
+      <IonRow>
+        <IonCol>{monthStart.getDay() == 0 ? number++ : ''}</IonCol>
+        <IonCol>
+          {monthStart.getDay() == 1 || number > 1 ? number++ : ''}
+        </IonCol>
+        <IonCol>
+          {monthStart.getDay() == 2 || number > 1 ? number++ : ''}
+        </IonCol>
+        <IonCol>
+          {monthStart.getDay() == 3 || number > 1 ? number++ : ''}
+        </IonCol>
+        <IonCol>
+          {monthStart.getDay() == 4 || number > 1 ? number++ : ''}
+        </IonCol>
+        <IonCol>
+          {monthStart.getDay() == 5 || number > 1 ? number++ : ''}
+        </IonCol>
+        <IonCol>
+          {monthStart.getDay() == 6 || number > 1 ? number++ : ''}
+        </IonCol>
+      </IonRow>
+      <IonRow>
+        <IonCol>{number++}</IonCol>
+        <IonCol>{number++}</IonCol>
+        <IonCol>{number++}</IonCol>
+        <IonCol>{number++}</IonCol>
+        <IonCol>{number++}</IonCol>
+        <IonCol>{number++}</IonCol>
+        <IonCol>{number++}</IonCol>
+      </IonRow>
+      <IonRow>
+        <IonCol>{number++}</IonCol>
+        <IonCol>{number++}</IonCol>
+        <IonCol>{number++}</IonCol>
+        <IonCol>{number++}</IonCol>
+        <IonCol>{number++}</IonCol>
+        <IonCol>{number++}</IonCol>
+        <IonCol>{number++}</IonCol>
+      </IonRow>
+      <IonRow>
+        <IonCol>{number++}</IonCol>
+        <IonCol>{number++}</IonCol>
+        <IonCol>{number++}</IonCol>
+        <IonCol>{number++}</IonCol>
+        <IonCol>{number++}</IonCol>
+        <IonCol>{number++}</IonCol>
+        <IonCol>{number++}</IonCol>
+      </IonRow>
+      <IonRow>
+        <IonCol>{number <= 31 ? number++ : ''}</IonCol>
+        <IonCol>{number <= 31 ? number++ : ''}</IonCol>
+        <IonCol>{number <= 31 ? number++ : ''}</IonCol>
+        <IonCol>{number <= 31 ? number++ : ''}</IonCol>
+        <IonCol>{number <= 31 ? number++ : ''}</IonCol>
+        <IonCol>{number <= 31 ? number++ : ''}</IonCol>
+        <IonCol>{number <= 31 ? number++ : ''}</IonCol>
+      </IonRow>
+    </IonGrid>
+  );
+};
+
+const CalendarLeafs: React.FC<object> = () => {
+  let calStart = false;
+
+  return <>{populateCalendar()}</>;
 };
 
 export default CalendarLeafs;
