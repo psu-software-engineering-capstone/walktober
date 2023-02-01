2523a13473a1e20ca770b8c5131818b152ec50cc
Nathan Moes <nathanmoes7@gmail.com>
2023-02-01 14:01:42 -0800

    comments on usage, and some minor style

--- src/components/CalendarLeafs.tsx
+++ src/components/CalendarLeafs.tsx
@@ -5,67 +5,140 @@ const currentDate = new Date();
 const currentYear = currentDate.getFullYear();
 const monthStart = new Date(`October 1, ${currentYear}`);
 
+// for displaying badges we could just make an array of the badge level with its current leaf. That way the content could
+// just be leaftDate[number]; to display it more or less. Probably more complicated than that but that's the jist
+
 const populateCalendar = () => {
   let number = 1;
 
   return (
     <IonGrid>
       <IonRow>
-        <IonCol>{monthStart.getDay() == 0 ? number++ : ''}</IonCol>
         <IonCol>
-          {monthStart.getDay() == 1 || number > 1 ? number++ : ''}
+          <IonItem>{monthStart.getDay() == 0 ? number++ : ''}</IonItem>
         </IonCol>
         <IonCol>
-          {monthStart.getDay() == 2 || number > 1 ? number++ : ''}
+          <IonItem>
+            {monthStart.getDay() == 1 || number > 1 ? number++ : ''}
+          </IonItem>
         </IonCol>
         <IonCol>
-          {monthStart.getDay() == 3 || number > 1 ? number++ : ''}
+          <IonItem>
+            {monthStart.getDay() == 2 || number > 1 ? number++ : ''}
+          </IonItem>
         </IonCol>
         <IonCol>
-          {monthStart.getDay() == 4 || number > 1 ? number++ : ''}
+          <IonItem>
+            {monthStart.getDay() == 3 || number > 1 ? number++ : ''}
+          </IonItem>
         </IonCol>
         <IonCol>
-          {monthStart.getDay() == 5 || number > 1 ? number++ : ''}
+          <IonItem>
+            {monthStart.getDay() == 4 || number > 1 ? number++ : ''}
+          </IonItem>
         </IonCol>
         <IonCol>
-          {monthStart.getDay() == 6 || number > 1 ? number++ : ''}
+          <IonItem>
+            {monthStart.getDay() == 5 || number > 1 ? number++ : ''}
+          </IonItem>
+        </IonCol>
+        <IonCol>
+          <IonItem>
+            {monthStart.getDay() == 6 || number > 1 ? number++ : ''}
+          </IonItem>
         </IonCol>
       </IonRow>
       <IonRow>
-        <IonCol>{number++}</IonCol>
-        <IonCol>{number++}</IonCol>
-        <IonCol>{number++}</IonCol>
-        <IonCol>{number++}</IonCol>
-        <IonCol>{number++}</IonCol>
-        <IonCol>{number++}</IonCol>
-        <IonCol>{number++}</IonCol>
+        <IonCol>
+          <IonItem>{number++}</IonItem>
+        </IonCol>
+        <IonCol>
+          <IonItem>{number++}</IonItem>
+        </IonCol>
+        <IonCol>
+          <IonItem>{number++}</IonItem>
+        </IonCol>
+        <IonCol>
+          <IonItem>{number++}</IonItem>
+        </IonCol>
+        <IonCol>
+          <IonItem>{number++}</IonItem>
+        </IonCol>
+        <IonCol>
+          <IonItem>{number++}</IonItem>
+        </IonCol>
+        <IonCol>
+          <IonItem>{number++}</IonItem>
+        </IonCol>
       </IonRow>
       <IonRow>
-        <IonCol>{number++}</IonCol>
-        <IonCol>{number++}</IonCol>
-        <IonCol>{number++}</IonCol>
-        <IonCol>{number++}</IonCol>
-        <IonCol>{number++}</IonCol>
-        <IonCol>{number++}</IonCol>
-        <IonCol>{number++}</IonCol>
+        <IonCol>
+          <IonItem>{number++}</IonItem>
+        </IonCol>
+        <IonCol>
+          <IonItem>{number++}</IonItem>
+        </IonCol>
+        <IonCol>
+          <IonItem>{number++}</IonItem>
+        </IonCol>
+        <IonCol>
+          <IonItem>{number++}</IonItem>
+        </IonCol>
+        <IonCol>
+          <IonItem>{number++}</IonItem>
+        </IonCol>
+        <IonCol>
+          <IonItem>{number++}</IonItem>
+        </IonCol>
+        <IonCol>
+          <IonItem>{number++}</IonItem>
+        </IonCol>
       </IonRow>
       <IonRow>
-        <IonCol>{number++}</IonCol>
-        <IonCol>{number++}</IonCol>
-        <IonCol>{number++}</IonCol>
-        <IonCol>{number++}</IonCol>
-        <IonCol>{number++}</IonCol>
-        <IonCol>{number++}</IonCol>
-        <IonCol>{number++}</IonCol>
+        <IonCol>
+          <IonItem>{number++}</IonItem>
+        </IonCol>
+        <IonCol>
+          <IonItem>{number++}</IonItem>
+        </IonCol>
+        <IonCol>
+          <IonItem>{number++}</IonItem>
+        </IonCol>
+        <IonCol>
+          <IonItem>{number++}</IonItem>
+        </IonCol>
+        <IonCol>
+          <IonItem>{number++}</IonItem>
+        </IonCol>
+        <IonCol>
+          <IonItem>{number++}</IonItem>
+        </IonCol>
+        <IonCol>
+          <IonItem>{number++}</IonItem>
+        </IonCol>
       </IonRow>
       <IonRow>
-        <IonCol>{number <= 31 ? number++ : ''}</IonCol>
-        <IonCol>{number <= 31 ? number++ : ''}</IonCol>
-        <IonCol>{number <= 31 ? number++ : ''}</IonCol>
-        <IonCol>{number <= 31 ? number++ : ''}</IonCol>
-        <IonCol>{number <= 31 ? number++ : ''}</IonCol>
-        <IonCol>{number <= 31 ? number++ : ''}</IonCol>
-        <IonCol>{number <= 31 ? number++ : ''}</IonCol>
+        <IonCol>
+          <IonItem>{number <= 31 ? number++ : ''}</IonItem>
+        </IonCol>
+        <IonCol>
+          <IonItem>{number <= 31 ? number++ : ''}</IonItem>
+        </IonCol>
+        <IonCol>
+          <IonItem>{number <= 31 ? number++ : ''}</IonItem>
+        </IonCol>
+        <IonCol>
+          <IonItem>{number <= 31 ? number++ : ''}</IonItem>
+        </IonCol>
+        <IonCol>
+          <IonItem>{number <= 31 ? number++ : ''}</IonItem>
+        </IonCol>
+        <IonCol>
+          <IonItem>{number <= 31 ? number++ : ''}</IonItem>
+        </IonCol>
+        <IonCol>
+          <IonItem>{number <= 31 ? number++ : ''}</IonItem>
+        </IonCol>
       </IonRow>
     </IonGrid>
   );
