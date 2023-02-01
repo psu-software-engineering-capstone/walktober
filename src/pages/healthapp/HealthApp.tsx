51e6d8b869011f6152e2fa86a4e0b5a03be35e0b
melis29 <melissa.nguyen187@gmail.com>
2023-01-31 16:41:56 -0800

    Pushed for Juhwan

--- src/pages/healthapp/HealthApp.tsx
+++ src/pages/healthapp/HealthApp.tsx
@@ -16,6 +16,7 @@ import { auth, FirestoreDB } from '../../firebase';
 import { doc } from 'firebase/firestore';
 import { useHistory } from 'react-router';
 import { updateDoc } from 'firebase/firestore';
+import { Health, HealthQueryOptions } from '@awesome-cordova-plugins/health';
 
 const HealthApp: React.FC = () => {
   const available = async () => {
@@ -109,7 +110,77 @@ const HealthApp: React.FC = () => {
       totalStep: totalStep
     });
   };
+//------------------------------------------------------------------------------------------------------------
+// const supportedTypes = [
+//   'TYPE_STEP_COUNT_DELTA'
+// ];
 
+const supportedTypes = [
+  'steps', 'distance',   // Read and write permissions
+  {
+    read : ['steps'],       // Read only permission
+    write : ['height', 'weight']  // Write only permission
+  }
+];
+
+const GFavailable = async () => {
+  await Health.isAvailable()
+    .then((data: any) => alert(JSON.stringify(data)))
+    .catch((error: any) => alert(JSON.stringify(error)));
+};
+
+const GFrequestAuthorization = async () => {
+  await Health.requestAuthorization(supportedTypes)
+    .then((data: any) => alert(JSON.stringify(data)))
+    .catch((error: any) => alert(JSON.stringify(error)));
+};
+
+const GFcheckAuthStatus = async () => {
+  Health.isAuthorized(supportedTypes)
+    .then((data: any) => alert(JSON.stringify(data)))
+    .catch((error: any) => alert(JSON.stringify(error)));
+};
+
+// const GFupdateCurrentUser = async (stepsByDate: any, totalStep: any) => {
+//   if (auth.currentUser == null) {
+//     alert('You are not logged in!');
+//     useHistory().push("/login");
+//     return;
+//   }
+//   const currentUserRef = doc(
+//     FirestoreDB,
+//     'users',
+//     auth.currentUser.email as string
+//   );
+//   await updateDoc(currentUserRef, {
+//     stepsByDate: stepsByDate,
+//     totalStep: totalStep
+//   });
+// };
+
+const GFupdateSteps = async () => {
+  const date = new Date();
+  const stepOptions : HealthQueryOptions = {
+    startDate: new Date(date.getFullYear(), date.getMonth(), 1),
+    endDate: new Date(),
+    dataType: 'steps'
+  };
+  await Health.query(stepOptions)
+    .then(async (data: any) => {
+      const stepsByDate = [];
+      let totalStep = 0;
+      for (let i = 0; i < data.length; i++) {
+        const current = data[i];
+        const date = current.startDate.toString().slice(0, 10);
+        const steps = current.quantity;
+        totalStep += current.quantity;
+        stepsByDate[i] = { date, steps };
+      }
+      await updateCurrentUser(stepsByDate, totalStep);
+      alert('Steps Updated!');
+    })
+    .catch((error: any) => alert(JSON.stringify(error)));
+};
   return (
     <IonPage>
       <IonHeader>
@@ -139,6 +210,18 @@ const HealthApp: React.FC = () => {
         <h2>Fitbit</h2>
         <h2>Garmin</h2>
         <h2>Google Fit</h2>
+        <IonButton expand="block" onClick={GFavailable}>
+          Health Available?
+        </IonButton>
+        <IonButton expand="block" onClick={GFrequestAuthorization}>
+          Request Auth
+        </IonButton>
+        <IonButton expand="block" onClick={GFcheckAuthStatus}>
+          Check Auth Statu
+        </IonButton>
+        <IonButton expand="block" onClick={GFupdateSteps}>
+          Update Step Count
+        </IonButton>
         <h2>Misfit</h2>
         <h2>Omron</h2>
         <h2>Polar</h2>
