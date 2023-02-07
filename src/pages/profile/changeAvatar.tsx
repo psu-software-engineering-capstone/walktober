03b68a49592721fcb63c8f642fdc5413979b741b
Michael Robertson <marobertson11@gmail.com>
2023-02-07 15:59:02 -0800

    Made some changes. Need the new Main

--- src/pages/profile/changeAvatar.tsx
+++ src/pages/profile/changeAvatar.tsx
@@ -1,24 +1,50 @@
-import { IonContent, IonPage, IonTitle } from '@ionic/react';
+import { IonButton, IonContent, IonPage, IonTitle } from '@ionic/react';
 import ExploreContainer from '../../components/ExploreContainer';
 import NavBar from '../../components/NavBar';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { getStorage, ref, uploadBytes } from 'firebase/storage';
+import { auth } from '../../firebase';
 import '../Tab1.css';
-// sample data for team leaderboard & individual leaderboard (uncomment when using) //
-// import { TeamData, IndividualData } from './Sample
+
 const changeAvatar: React.FC = () => {
-    return (
-      <IonPage>
-        <NavBar>
-          <IonTitle>Tab 1</IonTitle>
+  const storage = getStorage();
+  const fileName = auth.currentUser.Name.replace(/\s/g, '');
+  const imageRef = ref(storage, 'images/' + fileName + '.jpg');
+  const imageString = imageRef.fullPath;
+
+  /*uploadBytes(imageRef, file).then(() => {
+    console.log('Uploaded a blob or file!');
+    updateProfile(auth.currentUser, imageString);
+  });
+
+  updateProfile(auth.currentUser, {
+  profile_picture: imageRef;
+}).then(() => {
+  // Profile updated!
+  // ...
+}).catch((error) => {
+  // An error occurred
+  // ...
+});
+
+  <IonButton onClick={uploadBytes(imageRef, file)}> Submit </IonButton>*/
+
+  return (
+    <IonPage>
+      <NavBar>
+        <IonTitle>Change Profile Picture</IonTitle>
+      </NavBar>
+      <IonContent fullscreen>
+        <NavBar collapse="condense">
+          <IonTitle size="large">Change Profile Picture</IonTitle>
         </NavBar>
-        <IonContent fullscreen>
-          <NavBar collapse="condense">
-            <IonTitle size="large">Tab 1</IonTitle>
-          </NavBar>
-          <ExploreContainer name="Tab 1 page" />
-        </IonContent>
-      </IonPage>
-    );
-  };
-  
-  export default changeAvatar;
-  
+        <ExploreContainer name="Change Profile Picture" />
+        <img alt="User's image" src={imageString}></img>
+        <IonButton> File Upload</IonButton>
+        
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default changeAvatar;
