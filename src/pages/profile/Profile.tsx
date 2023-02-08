047dcb811061d642149ec6aa4a1e2250de367682
Alana Gilston <abluescarab@users.noreply.github.com>
2023-02-07 20:46:23 -0800

    Changed CSS class name; removed IonItem for consistent background color

--- src/pages/profile/Profile.tsx
+++ src/pages/profile/Profile.tsx
@@ -67,44 +67,42 @@ const Profile: React.FC = () => {
         </NavBar>
       </IonHeader>
       <IonContent>
-        <IonItem>
-          <IonGrid>
-            <IonRow>
-              <IonCol size="auto">
-                <IonText>
-                  <IonImg
-                    className="profile_pic"
-                    src={profilePic}
-                    alt="Profile picture for the user signed in"
-                  ></IonImg>
-                  <IonButton>Change Profile Picture</IonButton>
-                  <h2>{name}</h2>
-                  <p>
-                    {username}
-                    <IonButton fill="clear" size="small">
-                      Change Username
-                    </IonButton>
-                  </p>
-                  <p>{email}</p>
-                  <IonButton>Change Password</IonButton> <br></br>
-                  <IonButton>Change Health App Preferences</IonButton>
-                </IonText>
-              </IonCol>
-              <IonCol>
-                <IonText>
-                  <p>Joined on {joinDateString}</p>
-                  <p>{totalDistance} miles walked in total</p>
-                  <IonLabel>Step Goal: </IonLabel>
-                  <IonItem fill="outline">
-                    <IonInput value="10,000" size={100}></IonInput>
-                  </IonItem>
-                  <h6>Badges:</h6>
-                  {/* TODO: Put badges here */}
-                </IonText>
-              </IonCol>
-            </IonRow>
-          </IonGrid>
-        </IonItem>
+        <IonGrid>
+          <IonRow>
+            <IonCol size="auto">
+              <IonText>
+                <IonImg
+                  className="profile-pic"
+                  src={profilePic}
+                  alt="Profile picture for the user signed in"
+                ></IonImg>
+                <IonButton>Change Profile Picture</IonButton>
+                <h2>{name}</h2>
+                <p>
+                  {username}
+                  <IonButton fill="clear" size="small">
+                    Change Username
+                  </IonButton>
+                </p>
+                <p>{email}</p>
+                <IonButton>Change Password</IonButton> <br></br>
+                <IonButton>Change Health App Preferences</IonButton>
+              </IonText>
+            </IonCol>
+            <IonCol>
+              <IonText>
+                <p>Joined on {joinDateString}</p>
+                <p>{totalDistance} miles walked in total</p>
+                <IonLabel>Step Goal: </IonLabel>
+                <IonItem fill="outline">
+                  <IonInput value="10,000" size={100}></IonInput>
+                </IonItem>
+                <h6>Badges:</h6>
+                {/* TODO: Put badges here */}
+              </IonText>
+            </IonCol>
+          </IonRow>
+        </IonGrid>
       </IonContent>
     </IonPage>
   );
