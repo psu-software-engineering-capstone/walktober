82cb37252f9c7ec915399ef3ff372527aba3b7e9
Alissa Friel <90873349+xuvea@users.noreply.github.com>
2023-02-08 17:29:44 -0800

    16 create partial views for leaderboards (#146)
    * Basic leaderboard
    
    Made a leaderboard component that creates a leaderboard from a hard coded dataset. Sorts them based on the most steps.
    
    * Leaderboard Graph Update
    
    New barebones Leaderboard component that display's all users by their names, and is sorted by who has the most steps.
    
    * package lock update
    
    * Update package-lock.json
    
    * Formatting Changes
    
    Went through and formatted the codebase to be more uniform.
    
    * Update BarGraph.tsx
    
    * Leaderboard implemented
    
    Leaderboard functioning with scrollable chart. Added component to the homepage.
    
    * Update BarGraph.tsx
    
    * Small Styling Changes
    
    No longer has multiple scroll bars, and I have removes borders and ticks.
    
    * Update package-lock.json
    
    * Revert "Update package-lock.json"
    
    This reverts commit 0ce34fdac9bb9e05eba86d0810b072635dbea3a9.
    
    * Update package-lock.json
    
    * Component Name Change and Changed Data Calls
    
    Changed the component name to something more related to it's use. Also changed the data calls to different dummy data.
    
    * Update LeaderBoardChart.scss
    
    * Update LeaderBoardChart.tsx

---
+++ src/components/LeaderBoard/LeaderBoardChart.tsx
@@ -0,0 +1,110 @@
+import { IonContent, IonHeader } from '@ionic/react';
+import React, { useEffect, useState } from 'react';
+import './LeaderBoardChart.scss';
+import { Chart as ChartJS, registerables } from 'chart.js';
+import { Bar } from 'react-chartjs-2';
+//import { People } from '../../utils';
+import { IndividualData} from '../../pages/SampleData';
+
+ChartJS.register(...registerables);
+
+interface Data{
+  name: string;
+  profile_pic: string;
+  totalStep: number;
+  // team_id: number;
+  // name: string;
+  // steps: number;
+}
+
+const LeaderBoardChart: React.FC = () => {
+  const [data, setData] = useState(Array<Data>);
+
+  const chartData = ({
+    /*Sorts the data of all users by the amount of steps taken. Labels formed from the names
+     * of the user, and the bars are the number of steps the user took
+     */
+    labels: data.sort((a: any, b: any) => (a.totalStep > b.totalStep ? -1 : 1)).map(
+      (row) => row.name
+    ),
+    datasets: [
+      {
+        label: 'Steps',
+        data: data.sort((a: any, b: any) => (a.totalStep > b.totalStep ? -1 : 1)).map(
+          (col) => col.totalStep
+        )
+      }
+    ]
+  });
+  const chartOptions = {
+    indexAxis: 'y',
+    maintainAspectRatio: false,
+    responive: true,
+    scaleShowValues: true,
+    layout: {},
+    elements: {
+      borderWidth: 2
+    },
+    scales: {
+      x: {
+        title: {
+          display: false
+        },
+        offset: false,
+        grid: {
+          display: false
+        },
+        border: {
+          display: false
+        },
+        ticks: {
+          autoSkip: false,
+          display: false,
+          stepSize: 500
+        }
+      },
+      y: {
+        beginAtZero: true,
+        title: {
+          display: false
+        },
+        offset: true,
+        grid: {
+          display: false
+        },
+        border: {
+          display: false
+        },
+        ticks: {
+          autoSkip: false,
+          align: 'center'
+        }
+      }
+    }
+  };
+  const boxAjust = () => {
+    const box = document.querySelector('.box');
+    if (box != null) {
+      box.setAttribute('style', 'height: 1000px');
+      if (chartData.labels.length > 10) {
+        const newHeight = 500 + (chartData.labels.length - 10) * 50;
+        box.setAttribute('style', 'height: ' + newHeight.toString() + 'px');
+      }
+    }
+  };
+  useEffect(() => {
+    setData(IndividualData);
+    boxAjust();
+  }, []);
+
+  return (
+    <IonContent>
+      <IonHeader>LeaderBoard</IonHeader>
+      <IonContent class="box">
+        <Bar data={chartData} options={chartOptions}></Bar>
+      </IonContent>
+    </IonContent>
+  );
+};
+
+export default LeaderBoardChart;
