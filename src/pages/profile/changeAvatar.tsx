554f596cdd0f8c52ad8cb0d8205748f199001888
Michael Robertson <marobertson11@gmail.com>
2023-02-09 14:07:19 -0800

    grab main/other work

--- src/pages/profile/changeAvatar.tsx
+++ src/pages/profile/changeAvatar.tsx
@@ -1,6 +1,7 @@
 import {
   IonButton,
   IonContent,
+  IonHeader,
   IonItem,
   IonLabel,
   IonPage,
@@ -9,26 +10,34 @@ import {
 import ExploreContainer from '../../components/ExploreContainer';
 import NavBar from '../../components/NavBar';
 import { getStorage, ref, uploadBytes } from 'firebase/storage';
-import { auth } from '../../firebase';
-import '../Tab1.css';
-//import { file } from '@babel/types';
+import { auth, FirestoreDB } from '../../firebase';
 import { updateProfile } from 'firebase/auth';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { doc, getDoc } from 'firebase/firestore';
+import './Profile.css';
 
 const changeAvatar: React.FC = () => {
-  console.log("changeAvatar");
-  console.log(auth.currentUser.userData.name, "attempting");
   const storage = getStorage();
-  const setFileName = auth.currentUser.name;
-  const fileName = setFileName.replace(/\s/g, '');
-  const imageRef = ref(storage, 'images/' + fileName + '.jpg');
+  const [fName, setfName] = useState("");
+  let daName = "";
+  const imageRef = ref(storage, 'images/' + fName + '.jpg');
   const imageString = imageRef.fullPath;
   const [currentFile, setCurrentFile] = useState();
-
-  const selectFile = (event:any) => {
+  
+  const selectFile = (event: any) => {
     const { files } = event.target.files[0];
     setCurrentFile(files);
   };
+
+  async function getName(): Promise<void> {
+    const dbRef = doc(FirestoreDB, 'users', auth.currentUser.email as string);
+    const dbSnap = await getDoc(dbRef);
+    const userData = dbSnap.data();
+    daName = userData.name;
+    daName = daName.replace(/\s/g, '');
+    setfName(daName);
+    console.log(fName, "was the fName," , typeof fName,  setfName, daName, typeof daName, 'Attempted');
+  }
 
   async function upload() {
     if (!currentFile) {
@@ -54,19 +63,26 @@ const changeAvatar: React.FC = () => {
     }
   }
 
+  useEffect(() => {
+    getName();
+  }, []);
+
   //onChange={ev=> this.onFileChange(ev)}
   return (
     <IonPage>
-      <NavBar>
-        <IonTitle>Change Profile Picture</IonTitle>
-      </NavBar>
-      <IonContent fullscreen>
-        <NavBar collapse="condense">
-          <IonTitle size="large">Change Profile Picture</IonTitle>
+      <IonHeader>
+        <NavBar>
+          <IonTitle>Change Profile Picture</IonTitle>
         </NavBar>
+      </IonHeader>
+      <IonContent fullscreen>
         <ExploreContainer name="Change Profile Picture" />
-        <img alt="User's image" src={imageString}></img>
+        <IonItem>
+        <img alt="User's image" src={'../../assets/Walktober'}></img>
+        </IonItem>
+        <IonItem>
         <IonButton> File Upload</IonButton>
+        </IonItem>
         <IonItem>
           <IonLabel> Image </IonLabel>
           <input type="file" onChange={selectFile} />
