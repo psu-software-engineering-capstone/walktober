14c966619618400e510330055b3f415e8c424adf
RClift <rclift@pdx.edu>
2023-02-28 15:25:46 -0800

    removed admin modal

--- src/pages/admin/admin.tsx
+++ src/pages/admin/admin.tsx
@@ -5,22 +5,18 @@ import {
   IonCol,
   IonContent,
   IonDatetime,
-  IonDatetimeButton,
   IonGrid,
   IonHeader,
-  IonIcon,
   IonInput,
   IonItem,
   IonLabel,
   IonModal,
   IonPage,
   IonRow,
-  IonTextarea,
   IonTitle,
   IonToolbar
 } from '@ionic/react';
 import NavBar from '../../components/NavBar';
-import { closeCircleSharp } from 'ionicons/icons';
 import { useContext, useEffect, useState } from 'react';
 import AuthContext from '../../store/auth-context';
 import { useHistory } from 'react-router-dom';
@@ -39,7 +35,6 @@ const Admin: React.FC = () => {
   //used to open and close modals
   const [isOpenUser, setIsOpenUser] = useState(false);
   const [isOpenTeam, setIsOpenTeam] = useState(false);
-  const [isOpenAnnouncements, setIsOpenAnnouncements] = useState(false);
   const [isOpenReport, setIsOpenReport] = useState(false);
 
   // used to send new team sizes and team creation date to database
@@ -409,12 +404,7 @@ const Admin: React.FC = () => {
               </IonButton>
             </IonCol>
             <IonCol class="invis-grid-col">
-              <IonButton
-                onClick={() => setIsOpenAnnouncements(true)}
-                class="admin-button"
-                size="large"
-                expand="block"
-              >
+              <IonButton class="admin-button" size="large" expand="block">
                 Announcements
               </IonButton>
             </IonCol>
@@ -528,71 +518,6 @@ const Admin: React.FC = () => {
               Save Settings
             </IonButton>
 
-            <IonModal keepContentsMounted={true} backdropDismiss={false}>
-              <IonDatetime id="datetime"></IonDatetime>
-            </IonModal>
-          </IonContent>
-        </IonModal>
-
-        <IonModal isOpen={isOpenAnnouncements} backdropDismiss={false}>
-          <IonHeader class="modal-header">
-            <IonToolbar>
-              <IonTitle class="modal-title">Announcements</IonTitle>
-              <IonButtons slot="end">
-                <IonButton
-                  onClick={() => setIsOpenAnnouncements(false)}
-                  class="admin-close-modal"
-                >
-                  Close
-                </IonButton>
-              </IonButtons>
-            </IonToolbar>
-          </IonHeader>
-          <IonContent className="ion-padding" class="modal-content">
-            <IonItem>
-              <IonTextarea
-                placeholder="Type announcement message here"
-                autoGrow={true}
-              ></IonTextarea>
-            </IonItem>
-            <IonGrid>
-              <IonRow>
-                <IonCol size="5" class="invis-announcements">
-                  <IonButton>Post Announcement</IonButton>
-                </IonCol>
-                <IonCol size="7" class="invis-announcements">
-                  <IonButton>Schedule Announcement</IonButton>
-                  <IonDatetimeButton datetime="datetime"></IonDatetimeButton>
-                </IonCol>
-              </IonRow>
-            </IonGrid>
-            <IonGrid>
-              <IonRow class="header-row">
-                <IonCol size="4" class="admin-col">
-                  Time Scheduled
-                </IonCol>
-                <IonCol size="8" class="admin-col">
-                  Announcement Contents
-                </IonCol>
-                <IonCol size="4" class="admin-col">
-                  Delete
-                </IonCol>
-              </IonRow>
-              <IonRow>
-                <IonCol size="4" class="admin-col">
-                  January 20, 2023 7:00PM
-                </IonCol>
-                <IonCol size="8" class="admin-col">
-                  Challenge #1
-                </IonCol>
-                <IonCol size="4" class="admin-col">
-                  <IonButton size="small">
-                    <IonIcon slot="start" icon={closeCircleSharp}></IonIcon>
-                    Remove
-                  </IonButton>
-                </IonCol>
-              </IonRow>
-            </IonGrid>
             <IonModal keepContentsMounted={true} backdropDismiss={false}>
               <IonDatetime id="datetime"></IonDatetime>
             </IonModal>
