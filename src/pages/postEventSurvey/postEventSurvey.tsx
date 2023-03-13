7a2d1724db4a95867f5a108e8c638d47242b19e6
Anthony Pham <anthonyphamdd@gmail.com>
2023-03-12 17:53:09 -0700

    Survey saved to databse. Added question for what device was used.

--- src/pages/postEventSurvey/postEventSurvey.tsx
+++ src/pages/postEventSurvey/postEventSurvey.tsx
@@ -16,16 +16,44 @@ import './postEventSurvey.scss';
 import { useHistory } from 'react-router';
 import smallLogo from '../../assets/Walktober.png';
 import AdminContext from '../../store/admin-context';
+import { doc, setDoc } from '@firebase/firestore';
+import { auth, FirestoreDB } from '../../firebase';
 
 function PostEventSurvey() {
   const history = useHistory();
   const [showPostSurvey, setShowPostSurvey] = useState(false);
   const adminInfo = useContext(AdminContext);
 
+  const [hours, setHours] = useState('');
+  const [minutes, setMinutes] = useState('');
+  const [recUse, setRecUse] = useState('');
+  const [wellBeingOpinion, setWellBeingOpinion] = useState('');
+  const [wouldParticipateOpinion, setWouldParticipateOpinion] = useState('');
+  const [deviceUsed, setDeviceUsed] = useState('');
+  const [whyNotParticipate, setWhyNotParticipate] = useState('');
+  const [participatedEvents, setParticipatedEvents] = useState('');
+  const [walktoberIdeas, setWalktoberIdeas] = useState('');
+  const [walktoberFeedback, setWalktoberFeedback] = useState('');
+
+  const createExitQuestions = () => {
+    void setDoc(doc(FirestoreDB, 'exitQuestions', auth.currentUser.email as string), {
+      hoursPerWeek: hours,
+      minutesPerDay: minutes,
+      recUsage: recUse,
+      wellBeing: wellBeingOpinion,
+      wouldParticipateAgain: wouldParticipateOpinion,
+      device: deviceUsed,
+      wouldNotParticipateAgain: whyNotParticipate,
+      prevParticipatedEvents: participatedEvents,
+      futureIdeas: walktoberIdeas,
+      feedback: walktoberFeedback
+    });
+  };
+
   useEffect(() => {
     const today = new Date();
     const eventEnd = new Date(adminInfo.endDate);
-    setShowPostSurvey(today < eventEnd);
+    setShowPostSurvey(today > eventEnd);
   }, []);
 
   const hoursPhysicallyActive = {
@@ -43,8 +71,12 @@ function PostEventSurvey() {
   const participateAgain = {
     header: 'Please select the response that most accurately depicts your thoughts to the statement: I would participate in Walktober again!'
   };
+  const deviceWasUsed = {
+    header: 'Please select the type of device you used for Walktober!'
+  };
+
   const submitExitForm = () => {
-    // Backend take info
+    createExitQuestions();
     setShowPostSurvey(false);
     history.push('/app/results');
   };
@@ -60,7 +92,7 @@ function PostEventSurvey() {
           <IonList class="ion-no-padding">
               <IonItem className="modal-field">
                 <IonLabel position="floating">How many hours per week are you active?</IonLabel>
-                  <IonSelect interfaceOptions={hoursPhysicallyActive} interface="action-sheet" placeholder="Select One">
+                  <IonSelect interfaceOptions={hoursPhysicallyActive} interface="action-sheet" placeholder="Select One" onIonChange={(e) => setHours(e.target.value as string)}>
                     <IonSelectOption value="0">0</IonSelectOption>
                     <IonSelectOption value="1">1</IonSelectOption>
                     <IonSelectOption value="2">2</IonSelectOption>
@@ -74,7 +106,7 @@ function PostEventSurvey() {
 
               <IonItem className="modal-field">
                 <IonLabel position="floating">How many minutes of activity per day?</IonLabel>
-                  <IonSelect interfaceOptions={minutesPhysicallyActive} interface="action-sheet" placeholder="Select One">
+                  <IonSelect interfaceOptions={minutesPhysicallyActive} interface="action-sheet" placeholder="Select One" onIonChange={(e) => setMinutes(e.target.value as string)}>
                     <IonSelectOption value="0-15">0-15</IonSelectOption>
                     <IonSelectOption value="16-30">16-30</IonSelectOption>
                     <IonSelectOption value="31-45">31-45</IonSelectOption>
@@ -85,7 +117,7 @@ function PostEventSurvey() {
 
               <IonItem className="modal-field">
                 <IonLabel position="floating">How often do you use the PSU Rec Center?</IonLabel>
-                  <IonSelect interfaceOptions={recCenterUsage} interface="action-sheet" placeholder="Select One">
+                  <IonSelect interfaceOptions={recCenterUsage} interface="action-sheet" placeholder="Select One" onIonChange={(e) => setRecUse(e.target.value as string)}>
                     <IonSelectOption value="never">I have never used the Rec Center</IonSelectOption>
                     <IonSelectOption value="1-3">1-3 times total</IonSelectOption>
                     <IonSelectOption value="1-3-times-per-month">1-3 times per month</IonSelectOption>
@@ -96,7 +128,7 @@ function PostEventSurvey() {
 
               <IonItem className="modal-field">
                 <IonLabel position="floating">Participation in Walktober helped me improve my health and well-being.</IonLabel>
-                  <IonSelect interfaceOptions={wellBeing} interface="action-sheet" placeholder="Select One">
+                  <IonSelect interfaceOptions={wellBeing} interface="action-sheet" placeholder="Select One" onIonChange={(e) => setWellBeingOpinion(e.target.value as string)}>
                     <IonSelectOption value="1">Strongly Disagree</IonSelectOption>
                     <IonSelectOption value="2">Disagree</IonSelectOption>
                     <IonSelectOption value="3">Neither Agree Nor Disagree</IonSelectOption>
@@ -107,7 +139,7 @@ function PostEventSurvey() {
 
               <IonItem className="modal-field">
                 <IonLabel position="floating">I would participate in Walktober again.</IonLabel>
-                  <IonSelect interfaceOptions={participateAgain} interface="action-sheet" placeholder="Select One">
+                  <IonSelect interfaceOptions={participateAgain} interface="action-sheet" placeholder="Select One" onIonChange={(e) => setWouldParticipateOpinion(e.target.value as string)}>
                     <IonSelectOption value="1">Strongly Disagree</IonSelectOption>
                     <IonSelectOption value="2">Disagree</IonSelectOption>
                     <IonSelectOption value="3">Neither Agree Nor Disagree</IonSelectOption>
@@ -117,23 +149,48 @@ function PostEventSurvey() {
               </IonItem>
 
               <IonItem className="modal-field">
+                <IonLabel position="floating">What device was used for Walktober?</IonLabel>
+                  <IonSelect interfaceOptions={deviceWasUsed} interface="action-sheet" placeholder="Select One" onIonChange={(e) => setDeviceUsed(e.target.value as string)}>
+                    <IonSelectOption value="Apple">Apple</IonSelectOption>
+                    <IonSelectOption value="Android">Android</IonSelectOption>
+                  </IonSelect>
+              </IonItem>
+
+              <IonItem className="modal-field">
                   <IonLabel position="floating">If you would not participate in Walktober again, why not?</IonLabel>
-                  <IonInput></IonInput>
+                  <IonInput
+                    type="text"
+                    name="participateText"
+                    onIonChange={(e) => setWhyNotParticipate(e.target.value as string)}
+                  ></IonInput>
               </IonItem>
 
               <IonItem className="modal-field">
                   <IonLabel position="floating">Please list any walking events you participated in during Walktober.</IonLabel>
-                  <IonInput></IonInput>
+                  <IonInput
+                    type="text"
+                    name="participatedEvents"
+                    onIonChange={(e) => setParticipatedEvents(e.target.value as string)}
+                  ></IonInput>
               </IonItem>
 
               <IonItem className="modal-field">
                   <IonLabel position="floating">Please list any ideas for walking activities for future Walktober events.</IonLabel>
-                  <IonInput placeholder="In-person guided walks or remote walking activities you'd be interested in?"></IonInput>
+                  <IonInput 
+                    placeholder="In-person guided walks or remote walking activities you'd be interested in?"
+                    type="text"
+                    name="ideas"
+                    onIonChange={(e) => setWalktoberIdeas(e.target.value as string)}
+                  ></IonInput>
               </IonItem>
 
               <IonItem className="modal-field">
-                  <IonLabel position="floating">Please provite us with any other feedback on your Walktober experience.</IonLabel>
-                  <IonInput></IonInput>
+                  <IonLabel position="floating">Please provide us with any other feedback on your Walktober experience.</IonLabel>
+                  <IonInput
+                    type="text"
+                    name="feedback"
+                    onIonChange={(e) => setWalktoberFeedback(e.target.value as string)}
+                  ></IonInput>
               </IonItem>
             </IonList>
 
