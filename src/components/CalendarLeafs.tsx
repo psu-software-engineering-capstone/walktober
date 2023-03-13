983fa69299c94dea77889e2bd9868b90f52bf616
Nathan Moes <nathanmoes7@gmail.com>
2023-03-13 14:33:55 -0700

    removed esLint error

--- src/components/CalendarLeafs.tsx
+++ src/components/CalendarLeafs.tsx
@@ -1,4 +1,4 @@
-import { IonCol, IonGrid } from '@ionic/react';
+import { IonGrid } from '@ionic/react';
 import React from 'react';
 import OrangeLeaf from '../assets/OrangeLeaf.png';
 import YellowLeaf from '../assets/yellowLeaf.png';
