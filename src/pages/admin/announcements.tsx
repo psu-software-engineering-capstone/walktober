4ebfc479d4e4a5849792110937796eb8b03f2805
xuvea <90873349+xuvea@users.noreply.github.com>
2023-03-14 18:49:43 -0700

    Announcement page styling and images added
    Added images to the how to for better guidance on how to use dyno to make announcements/automated messages.

--- src/pages/admin/announcements.tsx
+++ src/pages/admin/announcements.tsx
@@ -1,6 +1,11 @@
 import {
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCardTitle,
   IonContent,
   IonHeader,
+  IonImg,
   IonItem,
   IonGrid,
   IonCol,
@@ -15,7 +20,22 @@ import { useContext, useEffect } from 'react';
 import AuthContext from '../../store/auth-context';
 import { useHistory } from 'react-router-dom';
 import './announcements.scss';
+import choose from '../../assets/announcementWalkthrough/serverselect.png';
+import manageserver from '../../assets/announcementWalkthrough/manageservers.png';
+import modules from '../../assets/announcementWalkthrough/moduleselect.png';
+import automsgpg from '../../assets/announcementWalkthrough/automessagepage.png';
+import automsgdd from '../../assets/announcementWalkthrough/automessagedropdown.png';
+import createmsg from '../../assets/announcementWalkthrough/createmessage.png';
+import msgname from '../../assets/announcementWalkthrough/msgname.png';
+import channelselect from '../../assets/announcementWalkthrough/channelselect.png';
+import repeatingmsg from '../../assets/announcementWalkthrough/repeatingmsg.png';
+import selectdate from '../../assets/announcementWalkthrough/selectdate.png';
+import msgoption from '../../assets/announcementWalkthrough/msgoption.png';
+import plainmsg from '../../assets/announcementWalkthrough/plainmsg.png';
+import embededoption from '../../assets/announcementWalkthrough/embededoptions.png';
 
+
+import { create } from 'ionicons/icons';
 const Announcements: React.FC = () => {
   
   const history = useHistory();
@@ -43,30 +63,114 @@ const Announcements: React.FC = () => {
         </NavBar>
       </IonHeader>
       <IonContent className='page-background'>
-        <IonHeader>How to Make Announcements In Discord Via Dyno</IonHeader>
+        <IonTitle className='page-title'>How to Make Announcements In Discord Via Dyno</IonTitle>
         <IonGrid>
           <IonRow>
-            <IonCol>
-              <IonList>
-                <IonTitle>Navigating To Auto Message Module</IonTitle>
-                <IonItem>1. Go to the following link  <IonRouterLink href="https://dyno.gg/" target="_blank"> dyno.gg</IonRouterLink></IonItem>
-                <IonItem>2. Login into Dyno with Discord account that has Admin priviges on your server</IonItem>
-                <IonItem>3. Click the Manage Servers Button in the top right.</IonItem>
-                <IonItem>4. Select the Server That you would like to make announcements in.</IonItem>
-                <IonItem>5. Open the dropdown menu for modules on the left side of the page.</IonItem>
-                <IonItem>6. Select the &quot;Auto Message&quot; module.</IonItem>
-              </IonList>
+            <IonCol
+            sizeXs='12'
+            sizeSm='12'
+            sizeMd='6'
+            sizeLg='6'
+            sizeXl='6'>
+              <IonCard>
+                <IonCardHeader>
+                  <IonCardTitle>Navigating To Auto Message Module</IonCardTitle>
+                </IonCardHeader>
+                <IonCardContent>
+                  <IonList>
+                    <IonItem>1. Go to the following link 
+                      <IonRouterLink className='link' href="https://dyno.gg/" target="_blank"> dyno.gg</IonRouterLink>
+                       .</IonItem>
+                    <IonItem>2. Login into Dyno with Discord account that has Admin priviges on your server.</IonItem>
+                    <IonItem>3. Click the <b> Manage Servers </b> Button in the top right.</IonItem>
+                    <IonImg
+                        src={manageserver}
+                        alt='Showing manage server button next to user profile image on dyno site.'>
+                    </IonImg>
+                    <IonItem>4. Select the Server That you would like to make announcements in.</IonItem>
+                    <IonImg
+                        src={choose}
+                        alt='Server list in dyno with walktober server in view.'>
+                    </IonImg>
+                    <IonItem>5. Click on Modules on the left side of the page, or open the dropdown menu next to it.</IonItem>
+                    <IonImg
+                        src={modules}
+                        alt=''>
+                    </IonImg>
+                    <IonItem>6. If you are on the modules page, click the settings button under Auto Message module .</IonItem>
+                    <IonImg
+                        src={automsgpg}
+                        alt=''>
+                    </IonImg>
+                    <IonItem>7. Select the &quot;Auto Message&quot; module .</IonItem>
+                    <IonImg
+                        src={automsgdd}
+                        alt=''>
+                    </IonImg>
+                  </IonList>
+                </IonCardContent>
+              </IonCard>
             </IonCol>
-            <IonCol>
-              <IonList>
-              <IonTitle>Creating Your Automated Announcement</IonTitle>
-                <IonItem>1. Click the Create Automessage button.</IonItem>
-                <IonItem>2. Name you Automated message.</IonItem>
-                <IonItem>3. Choose the Announcements channel to post the message to.</IonItem>
-                <IonItem>4. Choose to either post one time, or a repeating message.</IonItem>
-                <IonItem>5. Choose the time and date that you would like the message to go out.</IonItem>
-                <IonItem>6. Make a plain text announcement, or use the embed option to create a more custom message.</IonItem>
-              </IonList>
+            <IonCol
+            sizeXs='12'
+            sizeSm='12'
+            sizeMd='6'
+            sizeLg='6'
+            sizeXl='6'>
+              <IonCard>
+                <IonCardHeader>
+                  <IonCardTitle>Creating Your Automated Announcement</IonCardTitle>
+                </IonCardHeader>
+                <IonCardContent>
+                  <IonList>
+                    <IonItem>1. Click the Create Automessage button to start.</IonItem>
+                    <IonImg
+                        src={createmsg}
+                        alt=''>
+                    </IonImg>
+                    <IonItem>2. Name you Automated message, as it will be saved to dyno with this name.</IonItem>
+                    <IonImg
+                        src={msgname}
+                        alt=''>
+                    </IonImg>
+                    <IonItem>3. Choose the Announcements channel for the message to be posted to on discord.</IonItem>
+                    <IonImg
+                        src={channelselect}
+                        alt=''>
+                    </IonImg>
+                    <IonItem>4. Choose to either make the announcement one time, or a repeating. 
+                      If you choose repeating option it will ask for a number of hours between each posting.</IonItem>
+                    <IonImg
+                        src={repeatingmsg}
+                        alt=''>
+                    </IonImg>
+                    <IonItem>5. Choose the time and date that you would like the message to go out.</IonItem>
+                    <IonImg
+                        src={selectdate}
+                        alt=''>
+                    </IonImg>
+                    <IonItem>6. Make a plain text announcement, or use the embed option to create a more custom message. See 
+                      6a and 6b for a more detailed explanation.</IonItem>
+                    <IonImg
+                        src={msgoption}
+                        alt=''>
+                    </IonImg>
+                    <IonItem>6a. Choosing plain message lets you make a simple plain text announcement.</IonItem>
+                    <IonImg
+                        src={plainmsg}
+                        alt=''>
+                    </IonImg>
+                    <IonItem>6b. Choosing embeded message gives you more freedom for customizing your message. It 
+                      allows you to change things like the title, add a link to the title, add images and thumbnails, etc. 
+                    </IonItem>
+                    <IonImg
+                        src={embededoption}
+                        alt=''>
+                    </IonImg>
+                    <IonItem>7. Lastly, hit the save button to add the new announcement to your server!</IonItem>
+                  </IonList>
+                </IonCardContent>
+              </IonCard>
             </IonCol>
           </IonRow>
         </IonGrid>
