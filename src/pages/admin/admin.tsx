684a8a1cfef9045b5cd068d0b6b867d0a9f3d605
Michael Robertson <marobertson11@gmail.com>
2023-03-16 14:00:45 -0700

    changed status of admin open team into string instead of a  number

--- src/pages/admin/admin.tsx
+++ src/pages/admin/admin.tsx
@@ -188,7 +188,7 @@ const Admin: React.FC = () => {
         avg_steps: 0,
         leader: '',
         members: [],
-        status: 0,
+        status: '0',
         password: '',
         profile_pic:
           'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png',
